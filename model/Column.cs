80b8124fbeaee7387c1951b5670df33ef65d4e17
Keith Hall <kingkeith+github@gmail.com>
2015-12-14 16:24:41 +0200

    moved diffs to own folder

--- model/Column.cs
+++ model/Column.cs
@@ -146,34 +146,4 @@ namespace SchemaZen.model {
 			return SqlTypeToNativeType(Type);
 		}
 	}
-
-	public class ColumnDiff {
-		public Column Source;
-		public Column Target;
-
-		public ColumnDiff(Column target, Column source) {
-			Source = source;
-			Target = target;
-		}
-
-		public bool IsDiff {
-			get { return IsDiffBase || DefaultIsDiff; }
-		}
-
-		private bool IsDiffBase {
-			get {
-				return Source.IsNullable != Target.IsNullable || Source.Length != Target.Length ||
-				       Source.Position != Target.Position || Source.Type != Target.Type || Source.Precision != Target.Precision ||
-				       Source.Scale != Target.Scale || Source.ComputedDefinition != Target.ComputedDefinition;
-			}
-		}
-
-		public bool DefaultIsDiff {
-			get { return Source.DefaultText != Target.DefaultText; }
-		}
-
-		public bool OnlyDefaultIsDiff {
-			get { return DefaultIsDiff && !IsDiffBase; }
-		}
-	}
 }
---
+++ model/Diff/ColumnDiff.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SchemaZen.model
+{
+	public class ColumnDiff
+	{
+		public Column Source;
+		public Column Target;
+
+		public ColumnDiff(Column target, Column source)
+		{
+			Source = source;
+			Target = target;
+		}
+
+		public bool IsDiff
+		{
+			get { return IsDiffBase || DefaultIsDiff; }
+		}
+
+		private bool IsDiffBase
+		{
+			get
+			{
+				return Source.IsNullable != Target.IsNullable || Source.Length != Target.Length ||
+					   Source.Position != Target.Position || Source.Type != Target.Type || Source.Precision != Target.Precision ||
+					   Source.Scale != Target.Scale || Source.ComputedDefinition != Target.ComputedDefinition;
+			}
+		}
+
+		public bool DefaultIsDiff
+		{
+			get { return Source.DefaultText != Target.DefaultText; }
+		}
+
+		public bool OnlyDefaultIsDiff
+		{
+			get { return DefaultIsDiff && !IsDiffBase; }
+		}
+	}
+}
